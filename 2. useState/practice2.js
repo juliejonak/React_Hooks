4a5069141ebf87af5db7f1d4c461a1ef06b882b7
Julie Jonak <juliejonak10@gmail.com>
2020-02-07 20:19:19 -0800

    Adds practice file 2

---
+++ 2. useState/practice2.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+/*
+  INSTRUCTIONS:
+  Create a "todo" app with the following criteria.
+    1. The user can add new todo items
+    2. The user can remove todo items
+
+  Or use the practice sandbox here: https://codesandbox.io/s/usestate-practice-todos-vudl1?from-embed
+*/
+
+function Todo () {
+  return (
+    <div>
+      Follow the instructions above.
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Todo />, rootElement);
